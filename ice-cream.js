589e52780246798c044437ef48d5b52c1dea6424
Mikhail Serebriakov <mishacyb@gmail.com>
2020-04-07 21:49:51 +0000

    Add Ice Cream assignment file

---
+++ ice-cream.js
@@ -0,0 +1,18 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+
+// Add a flavor
+iceCreamFlavours.push("root beer");
+console.log(iceCreamFlavours);
+
+// Get the first flavor
+console.log(iceCreamFlavours[0]);
+
+// Get the last flavor
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+
+// Get total number of flavours
+console.log(iceCreamFlavours.length);
